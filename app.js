f3f1a3d2544c4bfe6584ca481368e31e2235cbd9
seanlanders92 <seanlanders92@gmail.com>
2020-01-07 15:01:34 -0800

    added questions in js

--- app.js
+++ app.js
@@ -2,3 +2,30 @@
 var greeting = prompt('Welcome! What is your name?');
 console.log(greeting);
 alert('Hello ' +greeting+ ' You will now be given a series of 5 questions to prove yourself!');
+
+var question1 = prompt('Is my name Sean?');
+if(question1.toLowerCase() === 'yes');{
+  console.log(question1);
+  alert('CORRECT! Now for a harder one!');
+}
+
+var question2 = prompt('Is my favorite game League of Legends?');
+if(question2.toLowerCase() === 'yes');{
+  console.log(question2);
+  alert('CORRECT! Now for a harder one!');
+}
+var question3 = prompt('Was I in the Army?');
+if(question3.toLowerCase() === 'yes');{
+  console.log(question3);
+  alert('CORRECT! Now for a harder one!');
+}
+var question4 = prompt('Am I from Virginia?');
+if(question4.toLowerCase() === 'yes');{
+  console.log(question4);
+  alert('CORRECT! Now for a harder one!');
+}
+var question5 = prompt('Did I drink cobra blood whisky while I was in the Philippines?');
+if(question5.toLowerCase() === 'yes');{
+  console.log(question5);
+  alert('CORRECT! Now for a harder one!');
+}
