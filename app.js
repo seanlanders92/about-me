2f810a81a25c48df190ef8b2dee8dc9c390e8231
seanlanders92 <seanlanders92@gmail.com>
2020-01-07 15:18:50 -0800

    added placeholder variables to the questions

--- app.js
+++ app.js
@@ -4,28 +4,35 @@ console.log(greeting);
 alert('Hello ' +greeting+ ' You will now be given a series of 5 questions to prove yourself!');
 
 var question1 = prompt('Is my name Sean?');
-if(question1.toLowerCase() === 'yes');{
-  console.log(question1);
+var low1 = question1.toLowerCase();
+
+if (low1 === 'yes' || low1 === 'y') {
   alert('CORRECT! Now for a harder one!');
+} else {
+  alert('Incorrect try again!');
 }
 
 var question2 = prompt('Is my favorite game League of Legends?');
-if(question2.toLowerCase() === 'yes');{
+var low2 = question2.toLowerCase();
+if (low2 === 'yes' || low2 === 'y'){
   console.log(question2);
-  alert('CORRECT! Now for a harder one!');
+  alert('CORRECT!');
 }
 var question3 = prompt('Was I in the Army?');
-if(question3.toLowerCase() === 'yes');{
+var low3 = question3.toLowerCase();
+if (low3 === 'yes' || low3 === 'y'){
   console.log(question3);
   alert('CORRECT! Now for a harder one!');
 }
 var question4 = prompt('Am I from Virginia?');
-if(question4.toLowerCase() === 'yes');{
+var low4 = question4.toLowerCase();
+if (low4 === 'yes' || low4 === 'y'){
   console.log(question4);
   alert('CORRECT! Now for a harder one!');
 }
 var question5 = prompt('Did I drink cobra blood whisky while I was in the Philippines?');
-if(question5.toLowerCase() === 'yes');{
+var low5 = question5.toLowerCase();
+if (low5 === 'yes' || low5 === 'y'){
   console.log(question5);
-  alert('CORRECT! Now for a harder one!');
+  alert('CORRECT! But there\'s no way you could have known that.');
 }
